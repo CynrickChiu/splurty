5689e5de8f3fb9905cfac8d44b4d18e3368117fe
Cynrick Chiu <cyn.2k5@outlook.com>
2015-05-17 01:36:42 +0000

    Add controller tests

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -11,4 +11,21 @@ class QuotesControllerTest < ActionController::TestCase
     get :show, :id => 'omg'
     assert_response :not_found
   end
+
+  test "create a quote success" do
+    assert_difference 'Quote.count' do
+      post :create, {:quote => {
+        :author => 'Dude',
+        :saying => 'For realz.'
+        }
+      }
+    end
+    assert_redirected_to root_path
+    assert_equal 1, Quote.count
+  end
+
+  test "create a quote validation error" do
+    quote = Quote.new(:author => '', :saying => '')
+    assert !quote.valid?
+  end
 end
